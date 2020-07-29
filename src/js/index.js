d572b14742750a6379192d7de44e36fa290f094a
Michal Samulak <sanchossjmistik@gmail.com>
2020-07-29 20:03:05 +0100

    set selectors and class

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,23 @@ import '../scss/main.scss';
 
 /* place your code below */
 
+
+const durationInput = document.querySelector('duration');
+const startBtn = document.querySelector('btn-start');
+const pauseBtn = document.querySelector('btn-pause');
+
+class Timer {
+    constructor(durationInput, startBtn, pauseBtn) {
+        this.durationInput = durationInput;
+        this.startBtn = startBtn;
+        this.pauseBtn = pauseBtn;
+
+        this.startBtn.addEventListener('click', this.start);
+    }
+
+    start() {
+        console.log('Timer started');
+    }
+}
+
+const timer = new Timer(durationInput, startBtn, pauseBtn);
