fcdcd3e81cbca23189354145ac53281c12d012d9
Michal Samulak <sanchossjmistik@gmail.com>
2020-08-06 13:39:06 +0100

    setup svg clock circle

--- src/js/index.js
+++ src/js/index.js
@@ -10,15 +10,26 @@ import Timer from "./Timer";
 const durationInput = document.querySelector(".duration");
 const startBtn = document.querySelector(".btn-start");
 const pauseBtn = document.querySelector(".btn-pause");
+const cirlce = document.querySelector(".clock_circle");
+
+const perimeter = cirlce.getAttribute('r') * 2 * Math.PI;
+cirlce.setAttribute('stroke-dasharray', perimeter);
+console.log(perimeter);
+
+let duration;
 const timer = new Timer(durationInput, startBtn, pauseBtn, {
-  onStart() {
-    console.log('Timer started');
+  onStart(totalDuration) {
+    duration = totalDuration;
   },
 
-  onTick() {
-    console.log('Timer ticked');
+  onTick(timeRemaining) {
+    cirlce.setAttribute('stroke-dashoffset', 
+      perimeter * timeRemaining / duration - perimeter
+    );
+
   },
   onComplete() {
-    console.log('Timer finish');
+    //let fin = 
+    durationInput.value = "30";
   }
 });
